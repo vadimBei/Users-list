e8c228db9d1eaa6bba4eae1931af51daac741663
Vadim <bey1705@gmail.com>
2020-09-13 16:49:15 +0300

    users-list component was added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatListModule } from '@angular/material/list'
+import { MatListModule } from '@angular/material/list';
+import { UsersListComponent } from './users-list/users-list.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent
   ],
   imports: [
     BrowserModule,
